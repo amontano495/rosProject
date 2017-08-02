62cf89fb4f8b3e984bd6a24377d3197497e159fa
Adam M Montano <amontano@056211h4pkjb2.rd.unr.edu>
2017-08-02 15:47:01 -0700

    modified:   simStart.sh

--- simStart.sh
+++ simStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#used to start SITL easily
 gnome-terminal --working-directory="/home/amontano/ardupilot/APMrover2" -e "sim_vehicle.py -j4 -L Quad --console --map"
 sleep 5
 gnome-terminal --working-directory="/home/amontano/ardupilot_ws/src" -e "roslaunch apm.launch"
